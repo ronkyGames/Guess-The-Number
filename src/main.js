6068d716048b9f28d0c798a3aefe1b91502372e5
Fabio Roncari <fabio.roncari@gmail.com>
2021-11-12 22:59:09 +0100

    Pop Up Game
    A pop up simple base game

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
-//Test Code to substitute
-const gameType = "awesome"
-alert(`Let's make ${ gameType } games!`)
+// Define and initialize the random variables numbers1 and numbers2
+let number1 = Math.floor(Math.random()*50)+1
+let number2 = Math.floor(Math.random()*50)+1
+
+// Define and initialize the variable guess that stores user input
+let guess = parseInt(`What is the result of ${ number1 } + ${ number2 }`,10)
+
+// Check if the answer is correct and tell the information to the user
+if((number1+number2) == guess){
+  alert("The answer is correct. You Win!")
+}else{
+  alert("The answer is wrong. You lose!")
+}
